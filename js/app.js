4a6b4e6f334c5a6231c16b844f6ddc0b76af1160
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-02-14 16:48:44 +0100

    EDIT card style

--- js/app.js
+++ js/app.js
@@ -49,13 +49,15 @@ for (let i = 0; i < teamPeople.length; i++) {
 
 	const teamHtml = 
     `
-    <div class="card">
-        <img src="./img/${avatar}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${fullName}</h5>
-            <p class="card-text">${role}</p>
+        <div class="card">
+            <img src="./img/${avatar}" class="card-img-top rounded mt-3" alt="#">
+            <div class="card-body">
+                <h5 class="card-title">${fullName}</h5>
+                <p class="card-text">${role}</p>
+            </div>
         </div>
-    </div>`
+
+    `
 
 	htmlTeamPrint.innerHTML += teamHtml
 }
